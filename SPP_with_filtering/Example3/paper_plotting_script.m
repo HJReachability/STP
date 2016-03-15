b2809e2cdfdbe57eb146e077bcfda887dee47f6d
Somil Bansal <somil@dhcp-44-109.EECS.Berkeley.EDU>
2016-03-15 11:58:06 -0700

    fixing figures

--- SPP_with_filtering/Example3/paper_plotting_script.m
+++ SPP_with_filtering/Example3/paper_plotting_script.m
@@ -80,73 +80,77 @@ set(gca,'YTickMode','manual');
 set(gca,'YTick',[-0.5, 0, 0.5]);
 set(gca,'XTickMode','manual');
 set(gca,'XTick',[-0.5, 0, 0.5]);
+set(gca, 'Fontsize', 14);
 axis equal;
 box on;
-legend('Targets', 'Positions, Headings', 'Danger Zones', 'Trajectories', 'Location','EastOutside');
+h_leg = legend('Targets', 'Positions, Headings', 'Danger Zones', 'Trajectories', 'Location','EastOutside');
+set(h_leg, 'Fontsize', 14);
 hold off;
 
-% %% Start working on Figure-2
-% % Time and vehicle to plot
-% t = [300, 180, 100, 2];
-% vehicle = 3;
-% figure,
-% f = gcf;
-% pos = get(f, 'position');
-% set(f, 'position', [pos(1) pos(2) 560 600]);
-% 
-% % Plot the target sets and initial conditions
-% for j=1:length(t) % Loop over time steps
-%     h{j} = subplot(2, 2, j);
-%     hold on;
-%     for i=vehicle:vehicle % Plot for the vehicle
-%         % Target set
-%         [g2D, data2D] = proj2D(g, allVehicles{i}.target, [0 0 1]);
-%         [~,h1] = contour(g2D.xs{1},g2D.xs{2}, data2D, [0 0], 'color', allVehicles{i}.fig_color,'linewidth',2);
-%         % Initial State
-%         plot(allVehicles{i}.x_nom(1,1), allVehicles{i}.x_nom(2,1), 'marker', '*', 'color', allVehicles{i}.fig_color,'markersize',5);
-%         % Arrow for initial state
-%         dirn = 0.2*[cos(allVehicles{i}.x_nom(3,1)) sin(allVehicles{i}.x_nom(3,1))];
-%         h2 = quiver(allVehicles{i}.x_nom(1,1), allVehicles{i}.x_nom(2,1), dirn(1), dirn(2), ...
-%             'maxheadsize', 50, 'marker', '*', 'color', allVehicles{i}.fig_color);
-%         % Reachable set
-%         [g2D, data2D] = proj2D(g, allVehicles{i}.reach(:,:,:,t(j)), [0 0 1], allVehicles{i}.x(3,1));
-%         [~, h3] = contour(g2D.xs{1},g2D.xs{2}, data2D, [0 0], 'color', allVehicles{i}.fig_color,'linestyle', '-');
-%         % Overall obstacle shape
-%         captureRadius = allVehicles{i}.capture_radius + 2*allVehicles{i}.bubble_radius;
-%         vnum = i-1;
-%         obstacle = 1e6*ones(g.shape);
-%         for k=1:vnum
-%             obs_pos = allVehicles{k}.collisionmat(1,t(j));
-%             if(obs_pos > 100)
-%                 continue;
-%             else
-%                 temp = sqrt((g.xs{1} - allVehicles{k}.collisionmat(1,t(j))).^2 +...
-%                     (g.xs{2} - allVehicles{k}.collisionmat(2,t(j))).^2) - captureRadius;
-%                 obstacle = shapeUnion(obstacle,temp);
-%             end
-%         end
-%         [g2D, data2D] = proj2D(g, obstacle, [0 0 1]);
-%         if any(data2D(:) < 0)
-%             [~, h4] = contour(g2D.xs{1},g2D.xs{2}, data2D, [0 0], 'color', 'k');
-%         end
-%         drawnow;
-%         axis equal;
-%     end
-%     str = sprintf('t = %s',num2str(-t_end + t(j)*0.01,'%.2f'));
-%     title(str);
-%     set(gca,'YTickMode','manual');
-%     set(gca,'YTick',[-0.5, 0, 0.5]);
-%     set(gca,'XTickMode','manual');
-%     set(gca,'XTick',[-0.5, 0, 0.5]);
-%     axis equal;
-%     box on;
-% end
-% legend([h1, h2, h3, h4], 'Targets', 'Initial pos. and heading', 'Reachable set', 'Obstacle', 'Location','SouthOutside');
-% % Move the bottom two subplots a bit higher
-% p = get(h{3}, 'pos');
-% p(2) = 0.2;
-% set(h{3}, 'pos', p);
-% 
-% p = get(h{4}, 'pos');
-% p(2) = 0.2;
-% set(h{4}, 'pos', p);
+%% Start working on Figure-2
+% Time and vehicle to plot
+t = [300, 180, 100, 2];
+vehicle = 3;
+figure,
+f = gcf;
+pos = get(f, 'position');
+set(f, 'position', [pos(1) pos(2) 560 600]);
+
+% Plot the target sets and initial conditions
+for j=1:length(t) % Loop over time steps
+    h{j} = subplot(2, 2, j);
+    hold on;
+    for i=vehicle:vehicle % Plot for the vehicle
+        % Target set
+        [g2D, data2D] = proj2D(g, allVehicles{i}.target, [0 0 1]);
+        [~,h1] = contour(g2D.xs{1},g2D.xs{2}, data2D, [0 0], 'color', allVehicles{i}.fig_color,'linewidth',2);
+        % Initial State
+        plot(allVehicles{i}.x_nom(1,1), allVehicles{i}.x_nom(2,1), 'marker', '*', 'color', allVehicles{i}.fig_color,'markersize',5);
+        % Arrow for initial state
+        dirn = 0.2*[cos(allVehicles{i}.x_nom(3,1)) sin(allVehicles{i}.x_nom(3,1))];
+        h2 = quiver(allVehicles{i}.x_nom(1,1), allVehicles{i}.x_nom(2,1), dirn(1), dirn(2), ...
+            'maxheadsize', 50, 'marker', '*', 'color', allVehicles{i}.fig_color);
+        % Reachable set
+        [g2D, data2D] = proj2D(g, allVehicles{i}.reach(:,:,:,t(j)), [0 0 1], allVehicles{i}.x(3,1));
+        [~, h3] = contour(g2D.xs{1},g2D.xs{2}, data2D, [0 0], 'color', allVehicles{i}.fig_color,'linestyle', '-');
+        % Overall obstacle shape
+        captureRadius = allVehicles{i}.capture_radius + 2*allVehicles{i}.bubble_radius;
+        vnum = i-1;
+        obstacle = 1e6*ones(g.shape);
+        for k=1:vnum
+            obs_pos = allVehicles{k}.collisionmat(1,t(j));
+            if(obs_pos > 100)
+                continue;
+            else
+                temp = sqrt((g.xs{1} - allVehicles{k}.collisionmat(1,t(j))).^2 +...
+                    (g.xs{2} - allVehicles{k}.collisionmat(2,t(j))).^2) - captureRadius;
+                obstacle = shapeUnion(obstacle,temp);
+            end
+        end
+        [g2D, data2D] = proj2D(g, obstacle, [0 0 1]);
+        if any(data2D(:) < 0)
+            [~, h4] = contour(g2D.xs{1},g2D.xs{2}, data2D, [0 0], 'color', 'k');
+        end
+        drawnow;
+        axis equal;
+    end
+    str = sprintf('t = %s',num2str(-t_end + t(j)*0.01,'%.2f'));
+    title(str);
+    set(gca,'YTickMode','manual');
+    set(gca,'YTick',[-0.5, 0, 0.5]);
+    set(gca,'XTickMode','manual');
+    set(gca,'XTick',[-0.5, 0, 0.5]);
+    set(gca, 'Fontsize', 14);
+    axis equal;
+    box on;
+end
+h_leg = legend([h1, h2, h3, h4], 'Targets', 'Initial pos. and heading', 'Reachable set', 'Obstacle', 'Location','SouthOutside');
+set(h_leg, 'Fontsize', 14);
+% Move the bottom two subplots a bit higher
+p = get(h{3}, 'pos');
+p(2) = 0.18;
+set(h{3}, 'pos', p);
+
+p = get(h{4}, 'pos');
+p(2) = 0.18;
+set(h{4}, 'pos', p);
