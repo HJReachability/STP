f76ce0778734bb45b4adbcb5ea7f4cb82e7c823e
Mo Chen <mochen72@berkeley.edu>
2016-05-21 15:39:23 -0700

    Added code for computing pursuit evasion backward reachable set

---
+++ code/compute_PE_BRS.m
@@ -0,0 +1,59 @@
+function [g, data] = compute_PE_BRS(Plane1, Plane2, PEmode, Rc, tau)
+% [g, data] = compute_PE_BRS(Plane1, Plane2, Rc, tau)
+%   Computes the backward reachable set for pursuit evasion
+%
+% Inputs:
+%   Plane1 - this plane
+%   Plane2 - other plane
+%   PEmode - set to 'evade' if Plane1 is evader
+%            set to 'pursue' if Plane1 is pursuer
+%   Rc     - capture radius
+%   tau    - time stamps
+%
+% Mo Chen, 2016-05-21
+
+%% Default capture Planes, PEmode, capture radius, and time stamps
+if nargin < 1
+  Plane1 = Plane([0 0 0], 1, 5);
+end
+
+if nargin < 2
+  Plane2 = Plane([0 0 0], 1, 5);
+end
+
+if nargin < 3
+  PEmode = 'evade';
+end
+
+if nargin < 4
+  Rc = 5;
+end
+
+if nargin < 5
+  IAT = 3;
+  dt = 0.1;
+  tau = 0:dt:IAT;
+end
+
+%% Grid
+grid_min = [-6; -10; 0];
+grid_max = [20; 10; 2*pi];
+N = [51; 51; 51];
+pdDim = 3;
+g = createGrid(grid_min, grid_max, N, pdDim);
+
+%% Copy over Plane parameters
+schemeData.grid = g;
+schemeData.vrange1 = Plane1.vrange;
+schemeData.vrange2 = Plane2.vrange;
+schemeData.w1Max = Plane1.wMax;
+schemeData.w2Max = Plane2.wMax;
+schemeData.dMax = Plane1.dMax + Plane2.dMax;
+schemeData.PEmode = PEmode;
+schemeData.hamFunc = @dubins3DPEham;
+schemeData.partialFunc = @dubins3DPEpartial;
+
+%% Initial condition and solution
+data0 = shapeCylinder(g, 3, [0; 0; 0], Rc);
+data = HJIPDE_solve(data0, tau, schemeData, 'zero');
+end
