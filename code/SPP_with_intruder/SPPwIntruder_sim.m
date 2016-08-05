c619f57d6bbaac6f2e9e8f6d9ed1464c426bb547
Mo Chen <mochen72@berkeley.edu>
2016-08-04 22:28:18 -0700

    wrote skeleton for simulation code

--- code/SPP_with_intruder/SPPwIntruder_sim.m
+++ code/SPP_with_intruder/SPPwIntruder_sim.m
@@ -1,41 +1,78 @@
 function SPPwIntruder_sim()
 % Simulation, but for now just testing collision avoidance BRS computation
 
-%% Problem parameters
-% Vehicle
-vRangeA = [0.1 1];
-wMaxA = 1;
-Rc = 0.1; % Capture radius
-dMaxA = [0.1 0.2];
-
-% Intruder
-vRangeB = vRangeA;
-wMaxB = wMaxA;
-dMaxB = dMaxA;
-
-schemeData.dynSys = ...
-  PlaneCAvoid([0; 0; 0], wMaxA, vRangeA, wMaxB, vRangeB, dMaxA, dMaxB);
-schemeData.uMode = 'max';
-schemeData.dMode = 'min';
-
-%% Grid and target set
-grid_min = [-0.15; -0.25; 0]; % Lower corner of computation domain
-grid_max = [0.25; 0.25; 2*pi];    % Upper corner of computation domain
+% Grid
+grid_min = [-1; -1; 0]; % Lower corner of computation domain
+grid_max = [1; 1; 2*pi];    % Upper corner of computation domain
 N = [101; 101; 101];         % Number of grid points per dimension
 pdDims = 3;               % 3rd dimension is periodic
-schemeData.grid = createGrid(grid_min, grid_max, N, pdDims);
+g = createGrid(grid_min, grid_max, N, pdDims);
 
-data0 = shapeCylinder(schemeData.grid, 3, [0; 0; 0], Rc);
+% Time
+%--> tau = ...
 
-%% Time stamps
-t0 = 0;
-dt = 0.01;
-tIAT = 0.1;
-tau = t0:dt:tIAT;
+%% Load vehicles with gradient of BRS1 in the data field
+%--> load ...
 
-%% Compute set
-extraArgs.visualize = true;
-data = HJIPDE_solve(data0, tau, schemeData, 'zero', extraArgs);
-save('CA.mat', 'schemeData', 'data')
+%% Initialize intruder
+%--> Q_intruder = ...
+tUpper = inf;
 
+%% Load safety reachable set
+%--> load ...
+%--> safety_vf = ...
+
+
+%% Safety values over time; each row is a vehicle, and each column is a time step
+safety_vals = zeros(length(Q), length(tau));
+safety_threshold = 0.5;
+intruder_arrived = false;
+
+%% Simulate
+for i = 1:length(tau)
+  % Move intruder
+  %--> Q_intruder.updateState(
+  
+  % Hide intruder if time is later than tUpper
+  if tau(i) > tUpper
+    Q_intruder.unplotPosition();
+  end
+  
+  % Check safety
+  for j = 1:length(Q)
+    % Compute safety value
+    safety_vals(i,j) = eval_u(g, safety_vf, Q_intruder.x-Q{j}.x);
+    
+    if tau(i) <= tUpper
+      %% Before intruder leaves airspace
+      if safety_vals(i,j) < safety_threshold
+        % Safety controller
+        % Record intruder's time of arrival and departure from the space
+        if ~intruder_arrived
+          tLower = tau(i);
+          tUpper = tLower + tIAT;
+          intruder_arrived = true;
+        end
+      else
+        % Liveness controller
+        deriv = eval_u(g, Q{j}.BRS1deriv, Q{j}.x);
+        u = Q{j}.optCtrl(tau(i), Q{j}.x, deriv, 'min');
+        %--> d = ...
+        Q{j}.updateState(u, dt, Q{j}.x, d);
+      end
+    else
+      %% After intruder leaves airspace
+      % Load or compute BRS2derivs
+      deriv = eval_u(g, Q{j}.BRS2deriv, Q{j}.x);
+      u = Q{j}.optCtrl(tau(i), Q{j}.x, deriv, 'min');
+      %--> d = ...
+      Q{j}.updateState(u, dt, Q{j}.x, d);
+    end
+  end
+  
+  % Plot vehicles
+  plotVehicles([{Q_intruder}; Q]);
+  
+  % Plot obstacles
+  plotObstacles(Q);
 end
