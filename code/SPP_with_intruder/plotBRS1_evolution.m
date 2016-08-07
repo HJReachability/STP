637269eba583a20586d22b68efc90df08c425d3e
Mo Chen <mochen72@berkeley.edu>
2016-08-07 10:18:05 -0700

    minor change

--- code/SPP_with_intruder/plotBRS1_evolution.m
+++ code/SPP_with_intruder/plotBRS1_evolution.m
@@ -3,7 +3,7 @@ function plotBRS1_evolution(Q, veh, schemeData)
 theta0 = Q{veh}.xhist(3,1);
 
 figure
-for i = 1:length(Q{veh}.data.BRS1_tau)
+for i = length(Q{veh}.data.BRS1_tau):-1:1
   % Plot BRS
   [g2D, BRS2D] = ...
     proj(schemeData.grid, Q{veh}.data.BRS1(:,:,:,i), [0 0 1], theta0);
@@ -11,9 +11,12 @@ for i = 1:length(Q{veh}.data.BRS1_tau)
   hold on
   
   % Plot obstacle at the same time
+  small = 1e-4;
   t = Q{veh}.data.BRS1_tau(i);
   for j = 1:veh-1
-    oInd = find(Q{j}.data.augFlatObsBRS_tau == t);
+    oInd = find(Q{j}.data.augFlatObsBRS_tau < t+small & ...
+      Q{j}.data.augFlatObsBRS_tau > t-small);
+    
     if ~isempty(oInd)
       [~, Obs2D] = proj(schemeData.grid, ...
         Q{j}.data.augFlatObsBRS(:,:,:,oInd), [0 0 1], theta0);
