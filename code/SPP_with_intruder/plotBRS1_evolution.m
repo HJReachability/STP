4d30d7b8d65b17944a8368ccf5039d13f50768c9
Mo Chen <mochen72@berkeley.edu>
2016-08-07 17:47:14 -0700

    changed plotBRS1_evolution to work with any obstacle field

--- code/SPP_with_intruder/plotBRS1_evolution.m
+++ code/SPP_with_intruder/plotBRS1_evolution.m
@@ -1,4 +1,7 @@
-function plotBRS1_evolution(Q, veh, schemeData)
+function plotBRS1_evolution(Q, veh, schemeData, obs_field)
+% plotBRS1_evolution(Q, veh, schemeData, obs_field)
+%     Plots the backward time-evolution of BRS1 and and the induced obstacles
+%     given in the field obs_field
 
 theta0 = Q{veh}.xhist(3,1);
 
@@ -10,20 +13,22 @@ for i = length(Q{veh}.data.BRS1_tau):-1:1
   visSetIm(g2D, BRS2D);
   hold on
   
-  % Plot obstacle at the same time
+  % Plot obstacle at each time step
   small = 1e-4;
   t = Q{veh}.data.BRS1_tau(i);
   for j = 1:veh-1
-    oInd = find(Q{j}.data.augFlatObsBRS_tau < t+small & ...
-      Q{j}.data.augFlatObsBRS_tau > t-small);
+    % Determine the corresponding time index in the obstacles
+    oInd = find(Q{j}.data.(sprintf('%s_tau', obs_field)) < t+small & ...
+      Q{j}.data.(sprintf('%s_tau', obs_field)) > t-small);
     
+    % If a time index is found, plot the obstacle at that time step
     if ~isempty(oInd)
       [~, Obs2D] = proj(schemeData.grid, ...
-        Q{j}.data.augFlatObsBRS(:,:,:,oInd), [0 0 1], theta0);
+        Q{j}.data.(obs_field)(:,:,:,oInd), [0 0 1], theta0);
       visSetIm(g2D, Obs2D, 'k');
     end
   end
-  
+  title(sprintf('t = %f', Q{veh}.data.BRS1_tau(i)))
   drawnow;
   hold off
 end
