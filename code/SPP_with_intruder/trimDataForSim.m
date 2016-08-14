a436dfe95994eb17811d45bf71e0c05e432eb5f8
Mo Chen <mochen72@berkeley.edu>
2016-08-13 22:23:04 -0700

    small bug

--- code/SPP_with_intruder/trimDataForSim.m
+++ code/SPP_with_intruder/trimDataForSim.m
@@ -5,7 +5,7 @@ function Q = trimDataForSim(Q, fieldsToRemove)
 for veh = 1:length(Q)
   for i = 1:length(fieldsToRemove)
     if isfield(Q{veh}.data, fieldsToRemove{i})
-      Q{veh} = rmfield(Q{veh}.data, fieldsToRemove{i});
+      Q{veh}.data = rmfield(Q{veh}.data, fieldsToRemove{i});
     end
   end
 end
