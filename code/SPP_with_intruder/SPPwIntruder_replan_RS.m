bda30a9de5c5797c9f7c86855eaf59882ff53de8
Mo Chen <mochen72@berkeley.edu>
2016-08-16 14:39:34 -0700

    minor changes

--- code/SPP_with_intruder/SPPwIntruder_replan_RS.m
+++ code/SPP_with_intruder/SPPwIntruder_replan_RS.m
@@ -53,53 +53,55 @@ numVeh = length(Q);
 
 %% Start the computation of reachable sets
 for veh=1:numVeh
-  schemeData.dynSys = Q{veh};
-  
-  %% Gather induced obstacles for FRS computation
-  % Assume there's no static obstacle
-  fprintf('Gathering obstacles for vehicle %d for FRS computation...\n', veh)
-  obstacles = ...
-    gatherObstacles(Q(1:veh-1), schemeData, tauFRS, 'cylObs3D', 'forward');
-  
-  %% Compute FRS to determine the ETA
-  if ~isfield(Q{veh}.data, 'ETA')
-    fprintf('Determining ETA for vehicle %d\n', veh)
-    Q{veh} = determineETA(Q{veh}, tauFRS, schemeData, obstacles);
+  if Q{veh}.data.replan
+    schemeData.dynSys = Q{veh};
     
-    [Q1, Q2, Q3, Q4] = Q{:};
-    save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
-  end
-  
-  %% Compute the BRS (BRS1) of the vehicle with the above obstacles
-  if ~isfield(Q{veh}.data, 'BRS1')
-    tauBRS = Q{veh}.data.FRS1_tau;
-    fprintf('Gathering obstacles for vehicle %d for BRS computation...\n', veh)
+    %% Gather induced obstacles for FRS computation
+    % Assume there's no static obstacle
+    fprintf('Gathering obstacles for vehicle %d for FRS computation...\n', veh)
     obstacles = ...
-      gatherObstacles(Q(1:veh-1), schemeData, tauBRS, 'cylObs3D', 'backward');
+      gatherObstacles(Q(1:veh-1), schemeData, tauFRS, 'cylObs3D', 'forward');
     
-    fprintf('Computing BRS1 for vehicle %d\n', veh)
-    Q{veh} = computeBRS1(Q{veh}, tauBRS, schemeData, obstacles);
+    %% Compute FRS to determine the ETA
+    if ~isfield(Q{veh}.data, 'ETA')
+      fprintf('Determining ETA for vehicle %d\n', veh)
+      Q{veh} = determineETA(Q{veh}, tauFRS, schemeData, obstacles);
+      
+      [Q1, Q2, Q3, Q4] = Q{:};
+      save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
+    end
     
-    [Q1, Q2, Q3, Q4] = Q{:};
-    save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
-  end
-  
-  %% Compute the nominal trajectories based on BRS1
-  if ~isfield(Q{veh}.data, 'nomTraj')
-    fprintf('Computing nominal trajectory for vehicle %d\n', veh)
-    Q{veh} = computeNomTraj(Q{veh}, schemeData);
+    %% Compute the BRS (BRS1) of the vehicle with the above obstacles
+    if ~isfield(Q{veh}.data, 'BRS1')
+      tauBRS = Q{veh}.data.FRS1_tau;
+      fprintf('Gathering obstacles for vehicle %d for BRS computation...\n',veh)
+      obstacles = ...
+        gatherObstacles(Q(1:veh-1), schemeData, tauBRS, 'cylObs3D', 'backward');
+      
+      fprintf('Computing BRS1 for vehicle %d\n', veh)
+      Q{veh} = computeBRS1(Q{veh}, tauBRS, schemeData, obstacles);
+      
+      [Q1, Q2, Q3, Q4] = Q{:};
+      save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
+    end
     
-    [Q1, Q2, Q3, Q4] = Q{:};
-    save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
-  end
-  
-  %% Compute t-IAT backward reachable set from flattened 3D obstacle
-  if ~isfield(Q{veh}.data, 'cylObsBRS')
-    fprintf('Augmenting obstacles for vehicle %d\n', veh)
-    Q{veh} = augmentObstacles(Q{veh}, schemeData, rawObsBRS);
+    %% Compute the nominal trajectories based on BRS1
+    if ~isfield(Q{veh}.data, 'nomTraj')
+      fprintf('Computing nominal trajectory for vehicle %d\n', veh)
+      Q{veh} = computeNomTraj(Q{veh}, schemeData);
+      
+      [Q1, Q2, Q3, Q4] = Q{:};
+      save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
+    end
     
-    [Q1, Q2, Q3, Q4] = Q{:};
-    save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
+    %% Compute t-IAT backward reachable set from flattened 3D obstacle
+    if ~isfield(Q{veh}.data, 'cylObsBRS')
+      fprintf('Augmenting obstacles for vehicle %d\n', veh)
+      Q{veh} = augmentObstacles(Q{veh}, schemeData, rawObsBRS);
+      
+      [Q1, Q2, Q3, Q4] = Q{:};
+      save(filename, 'Q1', 'Q2', 'Q3', 'Q4', 'schemeData', '-v7.3')
+    end
   end
 end
 
--- code/SPP_with_intruder/plotVehicles.m
+++ code/SPP_with_intruder/plotVehicles.m
@@ -14,16 +14,14 @@ for veh = 1:length(Q)
         Q{veh}.getPosition, capture_radius, '-', 'color', colors(veh,:));
     end
     
-    % Plot induced obstacle for vehicles 1 to 3
-    if veh < length(Q)
-      [g2D, data2D] = ...
-        proj(schemeData.grid, Q{veh}.data.cylObs3D(:,:,:,tInds{veh}), [0 0 1]);
-      if isempty(ho{veh})
-        ho{veh} = visSetIm(g2D, data2D, colors(veh, :));
-        ho{veh}.LineStyle = '--';
-      else
-        ho{veh}.ZData = data2D;
-      end
+    % Plot induced obstacles
+    [g2D, data2D] = ...
+      proj(schemeData.grid, Q{veh}.data.cylObs3D(:,:,:,tInds{veh}), [0 0 1]);
+    if isempty(ho{veh})
+      ho{veh} = visSetIm(g2D, data2D, colors(veh, :));
+      ho{veh}.LineStyle = '--';
+    else
+      ho{veh}.ZData = data2D;
     end
     
     % Plot nominal trajectory
