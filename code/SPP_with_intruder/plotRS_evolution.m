bfbc9887f61e3c09f5086234a6506fdfe52963c6
Mo Chen <mochen72@berkeley.edu>
2016-08-16 17:13:55 -0700

    can now plot reachable sets with any field using plotRS_evolution

--- code/SPP_with_intruder/plotRS_evolution.m
+++ code/SPP_with_intruder/plotRS_evolution.m
@@ -1,32 +1,26 @@
-function plotBRS1_evolution(Q, veh, schemeData, obs_field, save_png)
+function plotRS_evolution(Q, veh, schemeData, obs_field, RS_field)
 % plotBRS1_evolution(Q, veh, schemeData, obs_field)
 %     Plots the backward time-evolution of BRS1 and and the induced obstacles
 %     given in the field obs_field
 
 if nargin < 5
-  save_png = true;
-end
-
-if save_png
-  % For saving graphics
-  folder = sprintf('%s_%f', mfilename, now);
-  system(sprintf('mkdir %s', folder));
+  RS_field = 'BRS1';
 end
 
 theta0 = Q{veh}.xhist(3,1);
 
 figure
-for i = length(Q{veh}.data.BRS1_tau):-1:1
+for i = 1:length(Q{veh}.data.(sprintf('%s_tau', RS_field)))
   % Plot BRS
   [g2D, BRS2D] = ...
-    proj(schemeData.grid, Q{veh}.data.BRS1(:,:,:,i), [0 0 1], theta0);
+    proj(schemeData.grid, Q{veh}.data.(RS_field)(:,:,:,i), [0 0 1], theta0);
   visSetIm(g2D, BRS2D);
   hold on
   
   % Plot obstacle at each time step
   small = 1e-4;
-  t = Q{veh}.data.BRS1_tau(i);
-  
+  t = Q{veh}.data.sprintf('%s_tau', RS_field)(i);
+
   for j = 1:veh-1
     % Determine the corresponding time index in the obstacles
     oInd = find(Q{j}.data.(sprintf('%s_tau', obs_field)) < t+small & ...
@@ -39,13 +33,9 @@ for i = length(Q{veh}.data.BRS1_tau):-1:1
       visSetIm(g2D, Obs2D, 'k');
     end
   end
-  title(sprintf('t = %f', Q{veh}.data.BRS1_tau(i)))
+  title(sprintf('t = %f', Q{veh}.data.sprintf('%s_tau', RS_field)(i)))
   drawnow;
-  if save_png
-    export_fig(sprintf('%s/%d', folder, i), '-png', '-m2')
-  end
-  
   hold off
-  
+
 end
 end
