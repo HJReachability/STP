889f01f6ce84b20c173bffe7aea703072a8571c5
Mo Chen <mochen72@berkeley.edu>
2016-08-16 17:18:42 -0700

    re-enabled save_png to plotRS

--- code/SPP_with_intruder/plotRS_evolution.m
+++ code/SPP_with_intruder/plotRS_evolution.m
@@ -1,10 +1,20 @@
-function plotRS_evolution(Q, veh, schemeData, obs_field, RS_field)
+function plotRS_evolution(Q, veh, schemeData, obs_field, RS_field, save_png)
 % plotBRS1_evolution(Q, veh, schemeData, obs_field)
 %     Plots the backward time-evolution of BRS1 and and the induced obstacles
 %     given in the field obs_field
 
 if nargin < 5
   RS_field = 'BRS1';
+end
+
+if nargin < 6
+  save_png = true;
+end
+
+if save_png
+  % For saving graphics
+  folder = sprintf('%s_%f', mfilename, now);
+  system(sprintf('mkdir %s', folder));
 end
 
 theta0 = Q{veh}.xhist(3,1);
@@ -20,7 +30,7 @@ for i = 1:length(Q{veh}.data.(sprintf('%s_tau', RS_field)))
   % Plot obstacle at each time step
   small = 1e-4;
   t = Q{veh}.data.(sprintf('%s_tau', RS_field))(i);
-
+  
   for j = 1:veh-1
     % Determine the corresponding time index in the obstacles
     oInd = find(Q{j}.data.(sprintf('%s_tau', obs_field)) < t+small & ...
@@ -35,7 +45,11 @@ for i = 1:length(Q{veh}.data.(sprintf('%s_tau', RS_field)))
   end
   title(sprintf('t = %f', Q{veh}.data.(sprintf('%s_tau', RS_field))(i)))
   drawnow;
+  
+  if save_png
+    export_fig(sprintf('%s/%d', folder, i), '-png', '-m2')
+  end
   hold off
-
+  
 end
 end
