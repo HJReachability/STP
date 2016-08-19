9fd502ae53c50af23ded40e3a6956c5cc9d70e8b
Mo Chen <mochen72@berkeley.edu>
2016-08-19 13:06:52 -0700

    determineETA now centers grid between target and current state

--- code/SPP_with_intruder/determineETA.m
+++ code/SPP_with_intruder/determineETA.m
@@ -6,19 +6,37 @@ schemeData.uMode = 'max';
 schemeData.dMode = 'min';
 schemeData.tMode = 'forward';
 
-% Set obstacles
-extraArgs.obstacles = obstacles;
+% Center the grid between target and current vehicle state
+center = 0.5*(vehicle.data.targetCenter + vehicle.x);
+
+new_gmin = center - 0.5;
+new_gmin(3) = schemeData.grid.min(3); 
+new_gmax = center + 0.5;
+new_gmax(3) = schemeData.grid.max(3);
+new_g = createGrid(new_gmin, new_gmax, schemeData.grid.N, 3);
+old_g = schemeData.grid;
+schemeData.grid = new_g;
 
 % Min with target
 extraArgs.targets = shapeEllipsoid(schemeData.grid, vehicle.x, ...
   2.5*schemeData.grid.dx);
 
-% Computation should stop once it contains the initial state
-extraArgs.stopSetIntersect = vehicle.data.targetsm;
+% Set obstacles
+extraArgs.obstacles = zeros(size(obstacles));
+for i = 1:size(obstacles, 4)
+  extraArgs.obstacles(:,:,:,i) = ...
+    migrateGrid(old_g, obstacles(:,:,:,i), schemeData.grid);
+end
 
+% Computation should stop once it contains the initial state
+extraArgs.stopSetIntersect = ...
+  migrateGrid(old_g, vehicle.data.targetsm, schemeData.grid);
+
+% Compute the FRS
 extraArgs.visualize = true;
 [vehicle.data.FRS1, vehicle.data.FRS1_tau] = ...
   HJIPDE_solve(extraArgs.targets, tauFRS, schemeData, 'none', extraArgs);
 
+% Extract ETA
 vehicle.data.ETA = vehicle.data.FRS1_tau(end);
 end
