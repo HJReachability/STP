563e148a687155ae6b3b55791b3dd3ef9429e6f4
Mo Chen <mochen72@berkeley.edu>
2016-08-19 18:42:37 -0700

    FRS computation plots 2D slices instead

--- code/SPP_with_intruder/determineETA.m
+++ code/SPP_with_intruder/determineETA.m
@@ -34,6 +34,9 @@ extraArgs.stopSetIntersect = shapeCylinder(schemeData.grid, 3, ...
 
 % Compute the FRS
 extraArgs.visualize = true;
+extraArgs.plotData.plotDims = [1, 1, 0];
+extraArgs.plotData.projpt = vehicle.x(3);
+
 extraArgs.fig_filename = sprintf('FRS_%f_', now);
 [vehicle.data.FRS1, vehicle.data.FRS1_tau] = ...
   HJIPDE_solve(extraArgs.targets, tauFRS, schemeData, 'none', extraArgs);
