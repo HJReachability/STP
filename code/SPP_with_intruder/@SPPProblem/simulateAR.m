bf2203fbc1400c01ca8275988e622da1b7149dac
Mo Chen <mochen72@berkeley.edu>
2016-08-20 22:30:56 -0700

    typo

--- code/SPP_with_intruder/@SPPProblem/simulateAR.m
+++ code/SPP_with_intruder/@SPPProblem/simulateAR.m
@@ -105,7 +105,7 @@ for i = 1:length(tauAR)
     if ~isempty(tInd)
       Q{veh}.data.tauARmin = min(Q{veh}.data.tauARmin, tauAR(i));
       Q{veh}.data.tauARmax = max(Q{veh}.data.tauARmax, tauAR(i));
-      liveness_rel_x = Q{veh}.data.nomTraj(:,tInd}) - Q{veh}.x;
+      liveness_rel_x = Q{veh}.data.nomTraj(:,tInd) - Q{veh}.x;
       liveness_rel_x(1:2) = rotate2D(liveness_rel_x(1:2), -Q{veh}.x(3));
       deriv = eval_u(RTTRS.g, RTTRS.Deriv, liveness_rel_x);
       
