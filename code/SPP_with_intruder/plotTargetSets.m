b59a98f7cd585df8e748212766f4fe26854edf3f
Mo Chen <mochen72@berkeley.edu>
2016-08-23 19:07:40 -0700

    visualization bug

--- code/SPP_with_intruder/plotTargetSets.m
+++ code/SPP_with_intruder/plotTargetSets.m
@@ -1,9 +1,7 @@
 function plotTargetSets(Q, g2D, colors)
 
 for veh = 1:length(Q)
-  target2D = shapeSphere(g2D, Q{veh}.targetCenter, Q{veh}.targetR);
-  ht = visSetIm(g2D, target2D, colors(veh,:));
-  ht.LineWidth = 3;
+  plotDisk(Q{veh}.targetCenter, Q{veh}.targetR, 'linewidth', 3);
   hold on
 end
 
