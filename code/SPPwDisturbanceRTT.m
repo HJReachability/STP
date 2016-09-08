2fbae34efe124894bcfdeab268b82ccd8ba02558
Mo Chen <mochen72@berkeley.edu>
2016-09-07 17:13:16 -0700

    minor change

--- code/SPPwDisturbanceRTT.m
+++ code/SPPwDisturbanceRTT.m
@@ -1,32 +1,36 @@
-function SPPwDisturbanceRTT()
+function SPPwDisturbanceRTT(SPPP)
 % SPPwDisturbanceRTT()
 %     Solves the entire SPP with disturbances problem using the RTT method
 
-initStates = { ...
-  [-0.5; 0;  0]; ...
-  [ 0.5; 0; -pi]; ...
-  [-0.6; 0.6; -pi/4]; ...
-  [ 0.6; 0.6; -3*pi/4]};
-
-targetCenters = { ...
-  [ 0.7;  0.2; 0]; ...
-  [-0.7;  0.2; 0]; ...
-  [ 0.7; -0.7; 0]; ...
-  [-0.7; -0.7; 0]};
-
-targetR = 0.1;
-
-% Vehicle parameters
-vehParams.vRangeA = [0.5 1];
-vehParams.wMaxA = 1;
-vehParams.dMaxA = [0.1 0.2];
+if nargin < 1
+  initStates = { ...
+    [-0.5; 0;  0]; ...
+    [ 0.5; 0; -pi]; ...
+    [-0.6; 0.6; -pi/4]; ...
+    [ 0.6; 0.6; -3*pi/4]};
+  
+  targetCenters = { ...
+    [ 0.7;  0.2; 0]; ...
+    [-0.7;  0.2; 0]; ...
+    [ 0.7; -0.7; 0]; ...
+    [-0.7; -0.7; 0]};
+  
+  targetR = 0.1;
+  
+  % Vehicle parameters
+  vehParams.vRangeA = [0.5 1];
+  vehParams.wMaxA = 1;
+  vehParams.dMaxA = [0.1 0.2];
+  
+  SPPP = SPPProblem(initStates, targetCenters, targetR, vehParams);
+  fprintf('Enter any modifications to the SPPProblem...\n')
+  keyboard
+end
 
 % RTT parameters
 vReserved = [0.25 -0.25];
 wReserved = -0.4;
 trackingRadius = 0.075;
-
-SPPP = SPPProblem(initStates, targetCenters, targetR, vehParams);
 
 SPPP.computeRTTRS(vReserved, wReserved, trackingRadius);
 SPPP.computeNIRS;
--- code/SPPwIntruderRTT.m
+++ code/SPPwIntruderRTT.m
@@ -1,35 +1,36 @@
-function SPPwIntruderRTT()
+function SPPwIntruderRTT(SPPP)
 % SPPwDisturbanceRTT()
 %     Solves the entire SPP with disturbances problem using the RTT method
 
-initStates = { ...
-  [-0.5; 0;  0]; ...
-  [ 0.5; 0; -pi]; ...
-  [-0.6; 0.6; -pi/4]; ...
-  [ 0.6; 0.6; -3*pi/4]};
-
-targetCenters = { ...
-  [ 0.7;  0.2; 0]; ...
-  [-0.7;  0.2; 0]; ...
-  [ 0.7; -0.7; 0]; ...
-  [-0.7; -0.7; 0]};
-
-targetR = 0.15;
-
-% Vehicle parameters
-vehParams.vRangeA = [0.1 1];
-vehParams.wMaxA = 1;
-vehParams.dMaxA = [0.1 0.2];
+if nargin < 1
+  initStates = { ...
+    [-0.5; 0;  0]; ...
+    [ 0.5; 0; -pi]; ...
+    [-0.6; 0.6; -pi/4]; ...
+    [ 0.6; 0.6; -3*pi/4]};
+  
+  targetCenters = { ...
+    [ 0.7;  0.2; 0]; ...
+    [-0.7;  0.2; 0]; ...
+    [ 0.7; -0.7; 0]; ...
+    [-0.7; -0.7; 0]};
+  
+  targetR = 0.15;
+  
+  % Vehicle parameters
+  vehParams.vRangeA = [0.1 1];
+  vehParams.wMaxA = 1;
+  vehParams.dMaxA = [0.1 0.2];
+  
+  SPPP = SPPProblem(initStates, targetCenters, targetR, vehParams);
+  fprintf('Enter any modifications to the SPPProblem...\n')
+  keyboard
+end
 
 % RTT parameters
 vReserved = [0.3 -0.3];
 wReserved = -0.4;
 trackingRadius = 0.075;
-
-SPPP = SPPProblem(initStates, targetCenters, targetR, vehParams);
-
-fprintf('Enter any modifications to the SPPProblem...\n')
-keyboard
 
 SPPP.computeRTTRS(vReserved, wReserved, trackingRadius);
 SPPP.computeCARS;
