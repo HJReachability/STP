c3010ba206d523646904d676d1fd953095a6c7f9
Mo Chen <mochen72@berkeley.edu>
2016-11-03 21:55:03 -0700

    bug

--- code/updateObstacles.m
+++ code/updateObstacles.m
@@ -1,4 +1,4 @@
-function obstacles = updateObstacles(tau, obstacles, newObs)
+function obstacles = updateObstacles(tau, obstacles, newObs_tau, newObs)
 % obstacles = gatherObstacles(vehicles, schemeData, tau, obs_type)
 %     Gathers obstacles by combining obstacles in the field obs_type of each
 %     vehicle in the vehicles list
@@ -6,14 +6,14 @@ function obstacles = updateObstacles(tau, obstacles, newObs)
 small = 1e-4;
 
 % Determine time bound
-min_tau = max( min(newObs.tau), min(tau) ) - small;
-max_tau = min( max(newObs.tau), max(tau) ) + small;
+min_tau = max( min(newObs_tau), min(tau) ) - small;
+max_tau = min( max(newObs_tau), max(tau) ) + small;
 
 % Determine indices within the time bound
-global_tau_inds = obstacles.tau > min_tau & obstacles.tau < max_tau;
-obs_tau_inds = newObs.tau > min_tau & newObs.tau < max_tau;
+global_tau_inds = tau > min_tau & tau < max_tau;
+obs_tau_inds = newObs_tau > min_tau & newObs_tau < max_tau;
 
 % Take union with previous obstacles for obstacles within the time bound
-obstacles.data(:,:,:,global_tau_inds) = ...
-  min(obstacles.data(:,:,:,global_tau_inds), newObs.data(:,:,:,obs_tau_inds));
+obstacles(:,:,:,global_tau_inds) = ...
+  min(obstacles(:,:,:,global_tau_inds), newObs(:,:,:,obs_tau_inds));
 end
