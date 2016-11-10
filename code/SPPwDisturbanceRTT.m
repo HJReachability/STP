bbfb7afd005b84e62e8dd92610d3acc8a9fa9e02
Mo Chen <mochen72@berkeley.edu>
2016-11-09 22:47:28 -0800

    ^

--- code/SPPwDisturbanceRTT.m
+++ code/SPPwDisturbanceRTT.m
@@ -41,7 +41,7 @@ if nargin < 1
   % Vehicle parameters
   vehParams.vRangeA = [0.1 2.5];
   vehParams.wMaxA = 2;
-  vehParams.dMaxA = 0.3*[max(vehParams.vRangeA) vehParams.wMaxA];
+  vehParams.dMaxA = 0.35*[max(vehParams.vRangeA) vehParams.wMaxA];
   
   % Grid parameters
   gridParams.min = [0; 0; 0];
