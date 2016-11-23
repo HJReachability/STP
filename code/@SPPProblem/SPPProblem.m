cc7b14f432468ff55164ba3be3aea7ee32d8817f
Mo Chen <mochen72@berkeley.edu>
2016-11-22 17:13:02 -0800

    cleaning up SPPProblem setup

--- code/@SPPProblem/SPPProblem.m
+++ code/@SPPProblem/SPPProblem.m
@@ -62,8 +62,102 @@ classdef SPPProblem < handle
   
   methods
     %% Contructor
-    function obj = SPPProblem(initStates, targetCenters, targetR, vehParams, ...
-        grid_params)
+    function obj = SPPProblem(problem_name)
+      switch problem_name
+        case 'SF_dstb_11'
+    %% Vehicle
+    vehParams.vRangeA = [0.1 2.5];
+    vehParams.wMaxA = 2;
+    vehParams.dMaxA = [1.1 0];
+    
+    vehParams.vReserved = [1 -1.2];
+    vehParams.wReserved = -0.8;
+    
+    %% RTT tracking radius
+    trackingRadius = 3.5;
+    
+    %% Grid
+    gridParams.min = [-50; -50; 0];
+    gridParams.max = [500; 500; 2*pi];
+    gridParams.min = [111; 111; 11];
+
+    %% SPP Problem
+    obj.targetR = 10;
+    obj.tMin = -500;
+    obj.dt = 0.25;
+    obj.Rc = 1;
+    
+    %% Initial states
+    numVeh = 50;
+    obj.initStates = cell(numVeh, 1);
+    initState = [475; 200; 220*pi/180];
+    for i = 1:numVeh
+      obj.initStates{i} = initState;
+    end
+
+    %% Initial targets
+    targetCentersSet = { ...
+      [300; 400]; ...
+      [50; 175]; ...
+      [75; 25]; ...
+      [450; 25] ...
+      };
+
+    obj.targetCenters = cell(numVeh,1);
+    for i = 1:numVeh
+      target_ind = randi(length(targetCentersSet));
+      obj.targetCenters{i} = targetCentersSet{target_ind};
+    end
+    
+    %% Obstacles
+    % Financial District
+    Obs1 = shapeRectangleByCorners(obj.g2D, [300; 250], [350; 300]);
+
+    % Union Square
+    Obs2 = shapeRectangleByCorners(obj.g2D, [-25; -30], [25; 30]);
+    Obs2 = rotateData(obj.g2D, Obs2, 7.5*pi/180, [1 2], []);
+    Obs2 = shiftData(obj.g2D, Obs2, [325 185], [1 2]);
+    Obs2b = shapeHyperplaneByPoints(obj.g2D, [170 0; 400 230], ...
+      [0 500]);
+    Obs2 = shapeDifference(Obs2, Obs2b);
+
+    % City Hall
+    Obs3 = shapeRectangleByCorners(obj.g2D, [-25; -5], [25; 5]);
+    Obs3 = rotateData(obj.g2D, Obs3, 7.5*pi/180, [1 2], []);
+    Obs3 = shiftData(obj.g2D, Obs3, [170 65], [1 2]);
+
+    % Boundary
+    Obs4 = -shapeRectangleByCorners(obj.g2D, obj.g2D.min+5, obj.g2D.max-5);
+
+    obj.staticObs = min(Obs1, Obs2);
+    obj.staticObs = min(obj.staticObs, Obs3);
+    obj.staticObs = min(obj.staticObs, Obs4);
+
+    %% Plot setup
+    mapFile = 'map_earth.png';
+    obj.plotSetup();
+%     plotSPPP(mapFile, targetCentersSet, targetR, SPPP.g2D, staticObs, ...
+%       initState); 
+    
+    augStaticObs = addCRadius(obj.g2D, obj.staticObs, trackingRadius);
+    SPPP.augStaticObs = repmat(augStaticObs, ...
+      [1 1 gridParams.N(3) length(SPPP.tau)]);    
+    
+    if isfield(extraArgs, 'RTTRS_file')
+      load(RTTRS_file)
+      SPPP.setRTTRS(RTTRS);
+    end          
+        case 'SF_dstb_6'
+          params.trackingRadius = 0.5;
+          params.dMaxA = [1.1 0];
+          
+          
+        case 'BA_dstb_11'
+        case 'SF_intr_2'
+        case 'SF_intr_3'
+        otherwise
+          error('Unknown simulation name!')
+      end
       if nargin < 1
         initStates = { ...
           [-0.6; 0.2;  0]; ...
--- code/SPPwDisturbanceRTT.m
+++ code/SPPwDisturbanceRTT.m
@@ -22,86 +22,86 @@ if nargin < 1
 %   vehParams.wMaxA = 1;
 %   vehParams.dMaxA = [0.1 0.2];
 
-  numVeh = 50;
-  initStates = cell(numVeh, 1);
-  initState = [475; 200; 220*pi/180];
-  for i = 1:numVeh
-    initStates{i} = initState;
-  end
-  
-  targetCentersSet = { ...
-    [300; 400]; ...
-    [50; 175]; ...
-    [75; 25]; ...
-    [450; 25] ...
-    };
-  
-  targetCenters = cell(numVeh,1);
-  for i = 1:numVeh
-    target_ind = randi(length(targetCentersSet));
-    targetCenters{i} = targetCentersSet{target_ind};
-  end
-  
-  targetR = 10;
-  
-  % Vehicle parameters
-  vehParams.vRangeA = [0.1 2.5];
-  vehParams.wMaxA = 2;
-%   vehParams.dMaxA = [1.1 0]; % 11 m/s wind is "high wind" or "strong breeze"
-  vehParams.dMaxA = [0.6 0]; % 6 m/s wind is "moderate breeze"
-
-  % RTT parameters
-  vReserved = [1 -1.2];
-  wReserved = -0.8;
-%   trackingRadius = 3.5;
-  trackingRadius = 0.5;
-  
-  % Grid parameters
-  gridParams.min = [-50; -50; 0];
-  gridParams.max = [500; 500; 2*pi];
-  gridParams.N = [125; 125; 11];  
-  
-  SPPP = SPPProblem(initStates, targetCenters, targetR, vehParams, gridParams);
-  
-  SPPP.tMin = -500;
-  SPPP.dt = 0.25;
-  SPPP.Rc = 1;
-  SPPP.tau = SPPP.tMin:SPPP.dt:SPPP.tTarget;
-  
-  % Financial District
-  Obs1 = shapeRectangleByCorners(SPPP.g2D, [300; 250], [350; 300]);
-  
-  % Union Square
-  Obs2 = shapeRectangleByCorners(SPPP.g2D, [-25; -30], [25; 30]);
-  Obs2 = rotateData(SPPP.g2D, Obs2, 7.5*pi/180, [1 2], []);
-  Obs2 = shiftData(SPPP.g2D, Obs2, [325 185], [1 2]);
-  Obs2b = shapeHyperplaneByPoints(SPPP.g2D, [170 0; 400 230], ...
-    [0 500]);
-  Obs2 = shapeDifference(Obs2, Obs2b);
-  
-  % City Hall
-  Obs3 = shapeRectangleByCorners(SPPP.g2D, [-25; -5], [25; 5]);
-  Obs3 = rotateData(SPPP.g2D, Obs3, 7.5*pi/180, [1 2], []);
-  Obs3 = shiftData(SPPP.g2D, Obs3, [170 65], [1 2]);
-  
-  % Boundary
-  Obs4 = -shapeRectangleByCorners(SPPP.g2D, SPPP.g2D.min+1, SPPP.g2D.max-1);
-  
-  staticObs = min(Obs1, Obs2);
-  staticObs = min(staticObs, Obs3);
-  staticObs = min(staticObs, Obs4);
-  SPPP.staticObs = staticObs;
-  
-  % Plot setup
-  mapFile = 'map_earth.png';
-  plotSPPP(mapFile, targetCentersSet, targetR, SPPP.g2D, staticObs, initState);
-  
-  augStaticObs = addCRadius(SPPP.g2D, staticObs, trackingRadius);
-  SPPP.augStaticObs = repmat(augStaticObs, ...
-    [1 1 gridParams.N(3) length(SPPP.tau)]);
-  
-  fprintf('Enter any modifications to the SPPProblem...\n')
-  keyboard
+%   numVeh = 50;
+%   initStates = cell(numVeh, 1);
+%   initState = [475; 200; 220*pi/180];
+%   for i = 1:numVeh
+%     initStates{i} = initState;
+%   end
+%   
+%   targetCentersSet = { ...
+%     [300; 400]; ...
+%     [50; 175]; ...
+%     [75; 25]; ...
+%     [450; 25] ...
+%     };
+%   
+%   targetCenters = cell(numVeh,1);
+%   for i = 1:numVeh
+%     target_ind = randi(length(targetCentersSet));
+%     targetCenters{i} = targetCentersSet{target_ind};
+%   end
+%   
+%   targetR = 10;
+%   
+%   % Vehicle parameters
+%   vehParams.vRangeA = [0.1 2.5];
+%   vehParams.wMaxA = 2;
+% %   vehParams.dMaxA = [1.1 0]; % 11 m/s wind is "high wind" or "strong breeze"
+%   vehParams.dMaxA = [0.6 0]; % 6 m/s wind is "moderate breeze"
+% 
+%   % RTT parameters
+%   vReserved = [1 -1.2];
+%   wReserved = -0.8;
+% %   trackingRadius = 3.5;
+%   trackingRadius = 0.5;
+%   
+%   % Grid parameters
+%   gridParams.min = [-50; -50; 0];
+%   gridParams.max = [500; 500; 2*pi];
+%   gridParams.N = [125; 125; 11];  
+%   
+%   SPPP = SPPProblem(initStates, targetCenters, targetR, vehParams, gridParams);
+%   
+%   SPPP.tMin = -500;
+%   SPPP.dt = 0.25;
+%   SPPP.Rc = 1;
+%   SPPP.tau = SPPP.tMin:SPPP.dt:SPPP.tTarget;
+%   
+%   % Financial District
+%   Obs1 = shapeRectangleByCorners(SPPP.g2D, [300; 250], [350; 300]);
+%   
+%   % Union Square
+%   Obs2 = shapeRectangleByCorners(SPPP.g2D, [-25; -30], [25; 30]);
+%   Obs2 = rotateData(SPPP.g2D, Obs2, 7.5*pi/180, [1 2], []);
+%   Obs2 = shiftData(SPPP.g2D, Obs2, [325 185], [1 2]);
+%   Obs2b = shapeHyperplaneByPoints(SPPP.g2D, [170 0; 400 230], ...
+%     [0 500]);
+%   Obs2 = shapeDifference(Obs2, Obs2b);
+%   
+%   % City Hall
+%   Obs3 = shapeRectangleByCorners(SPPP.g2D, [-25; -5], [25; 5]);
+%   Obs3 = rotateData(SPPP.g2D, Obs3, 7.5*pi/180, [1 2], []);
+%   Obs3 = shiftData(SPPP.g2D, Obs3, [170 65], [1 2]);
+%   
+%   % Boundary
+%   Obs4 = -shapeRectangleByCorners(SPPP.g2D, SPPP.g2D.min+5, SPPP.g2D.max-5);
+%   
+%   staticObs = min(Obs1, Obs2);
+%   staticObs = min(staticObs, Obs3);
+%   staticObs = min(staticObs, Obs4);
+%   SPPP.staticObs = staticObs;
+%   
+%   % Plot setup
+%   mapFile = 'map_earth.png';
+%   plotSPPP(mapFile, targetCentersSet, targetR, SPPP.g2D, staticObs, initState);
+%   
+%   augStaticObs = addCRadius(SPPP.g2D, staticObs, trackingRadius);
+%   SPPP.augStaticObs = repmat(augStaticObs, ...
+%     [1 1 gridParams.N(3) length(SPPP.tau)]);
+%   
+%   fprintf('Enter any modifications to the SPPProblem...\n')
+%   keyboard
 end
 
 % % RTT parameters for ACC and TCST papers
