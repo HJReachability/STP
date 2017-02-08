38c75ceb2e2b8357f9cdedf839ae27bff24ebe55
Mo Chen <mochen72@berkeley.edu>
2017-02-08 10:51:06 -0800

    added intruder simulation figure

--- code/avoid_intr_ctrl.m
+++ code/avoid_intr_ctrl.m
@@ -4,8 +4,10 @@ function [u, d] = avoid_intr_ctrl(evader, pursuer, CARS, CARSderiv)
 
 x_rel = PlaneDubins_relState(evader.x, pursuer.x);
 
-if any(x_rel' >= CARS.g.max) || any(x_rel' <= CARS.g.min)
+if any(x_rel(1:2)' >= CARS.g.max(1:2)) || any(x_rel(1:2)' <= CARS.g.min(1:2))
   u = [0; 0];
+  d = zeros(5,1);
+  d(1) = max(pursuer.vrange);
 else
   p = eval_u(CARS.g, CARSderiv, x_rel);
   u = CARS.dynSys.optCtrl([], x_rel, p, 'max');
--- code/simulateIntruder.m
+++ code/simulateIntruder.m
@@ -1,57 +1,91 @@
-function simulateIntruder(obj)
+function simulateIntruder(SPPP, Qthis)
 
-
-
-load(obj.CARS_filename)
-
-x0I = [0; 0; 0]; % (x, y) position is irrelevant at this point
-Qintr = SPPPlane(x0I, CARS.dynSys.wMaxB, CARS.vRangeB, CARS.dMaxB);
+load(SPPP.CARS_filename)
 
 % Index at which intruder appears
 intr_tInd = 100;
 figure;
-plot(nomTraj(1,intr_tInd), nomTraj(2,intr_tInd), 'k.')
+x0 = Qthis.xhist(:,intr_tInd);
+Qnew = Plane(x0, Qthis.wMax, Qthis.vrange, Qthis.dMax);
+plot(x0(1), x0(2), 'k.')
 hold on
 
-plotCARS(Qthis, Qintr, CARS);
-extraArgsE.color = 'k';
+% Intruder
+x0I = [0; 0; x0(3)-0.95*pi]; % (x, y) position is irrelevant at this point
+Qintr = SPPPlane(x0I, CARS.dynSys.wMaxB, CARS.dynSys.vRangeB, ...
+  CARS.dynSys.dMaxB);
+
+plotCARS(Qnew, Qintr, CARS);
+extraArgsE.Color = 'k';
+extraArgsE.LineWidth = 2;
 extraArgsI = extraArgsE;
-extraArgsI.color = 'r';
+extraArgsI.Color = 'r';
 
-Qthis.plotPosition(extraArgsE)
-Qintr.plotPosition(extraArgsI)
+Qnew.plotPosition(extraArgsE)
 
-[x, y] = ginput(1);
+xlim([x0(1)-30 x0(1)+30])
+ylim([x0(2)-30 x0(2)+30])
+
+[~, phist_orig] = Qthis.getPosition;
+plot(phist_orig(1,:), phist_orig(2,:), 'k.-')
+drawnow;
+
+% [x, y] = ginput(1)
+x = 411.5268;
+y = 159.9875;
+
+% Initialize intruder and new instance of plane
 Qintr.x(1:2) = [x; y];
+Qintr.xhist(1:2) = [x; y];
+Qintr.plotPosition(extraArgsI)
 drawnow;
 
 %% Simulate
 tMax = 10;
-dt = obj.dt;
+dt = SPPP.dt;
 t = 0:dt:tMax;
 
 for i = 2:length(t)
   % Find correct index for CARS and compute gradient
-  x_rel = PlaneDubins_relState(evader.x, pursuer.x);
+  x_rel = PlaneDubins_relState(Qnew.x, Qintr.x);
   tE = find_earliest_BRS_ind(CARS.g, flip(CARS.data,4), x_rel);
   tE = size(CARS.data, 4) - tE + 1;
   CARSderiv = computeGradients(CARS.g, CARS.data(:,:,:,tE));
   
   % Synthesize control and disturbance
-  [uE, d] = avoid_intr_ctrl(Qthis, Qintr, CARS, CARSderiv);
+  [uE, d] = avoid_intr_ctrl(Qnew, Qintr, CARS, CARSderiv);
   dE = -d(3:5)/2;
-  uI = d(1:2);
+  uI = [d(1); 0];d(1:2);
   dI = d(3:5)/2;
   
-  Qthis.updateState(uE, dt, Qthis.x, dE);
+  Qnew.updateState(uE, dt, Qnew.x, dE);
   Qintr.updateState(uI, dt, Qintr.x, dI);
   
-  Qthis.plotPosition(extraArgsE)
+  Qnew.plotPosition(extraArgsE)
   Qintr.plotPosition(extraArgsI)
   drawnow
 end
 
 %% Plot distance to nominal trajectory
+[~, phist_intr] = Qintr.getPosition;
+[~, phist_new] = Qnew.getPosition;
+phist_orig = phist_orig(:, intr_tInd+1:intr_tInd+length(t));
 
+dist_new = 10*sqrt((phist_new(1,:) - phist_intr(1,:)).^2 + ...
+  (phist_new(2,:) - phist_intr(2,:)).^2);
+
+dist_orig = 10*sqrt((phist_orig(1,:) - phist_intr(1,:)).^2 + ...
+  (phist_orig(2,:) - phist_intr(2,:)).^2);
+
+figure;
+h_rc = plot(t, 10*SPPP.Rc*ones(size(t)), 'k--');
+hold on
+h_new = plot(t, dist_new, 'b*-');
+
+h_orig = plot(t, dist_orig, 'r.-');
+xlabel('time (s)')
+ylabel('distance to intruder (m)')
+legend([h_rc h_new h_orig], {'Collision', 'Avoiding', 'Not avoiding'}, ...
+  'Location', 'best')
 
 end
