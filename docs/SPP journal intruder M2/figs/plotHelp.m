e96ccea0e1fc1f3b97f9fccdcff789ef2de52e51
Somil Bansal <somil@eecs.berkeley.edu>
2017-04-19 13:49:26 -0700

    Almost complete first draft :)

--- docs/SPP journal intruder M2/figs/plotHelp.m
+++ docs/SPP journal intruder M2/figs/plotHelp.m
@@ -2,5 +2,7 @@
 set(gca, 'fontsize', 14);
 
 % find all lines and increase their size
-set(findall(gca, 'Type', 'Line'),'LineWidth',3.5);
+set(findall(gca, 'Type', 'Line'),'LineWidth',0.5,'Linestyle', '--');
 % set(findall(gca, 'Type', 'Line'),'Linestyle', '--', 'LineWidth', 3.5);
+
+delete(findall(gca,'Type','text'))
